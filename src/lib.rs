a13ec68bfe78242da2f3a9ea0a582ecee77e0b12
Jakub Gladysz <jakub.gladysz@protonmail.com>
2025-08-13 20:20:39 +0200

    Fix wordlist generation function

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,50 @@
 use bip39::Mnemonic;
+use bitcoin::Address;
+use bitcoin::hashes::Hash;
 use rand::rngs::OsRng;
 use rand::{RngCore, TryRngCore};
 use std::path::PathBuf;
-use bitcoin::Address;
 
-struct Wallet {
+pub struct Wallet {
     name: String,
-    key_pair: KeyPair
+    key_pair: KeyPair,
 }
 
 impl Wallet {
     fn new(name: String, key_pair: KeyPair) -> Wallet {
-        Wallet {
-            name,
-            key_pair,
-        }
+        Wallet { name, key_pair }
     }
 
-    fn generate_recovery_code() -> Option<String> {
-        let entropy = get_entropy(Entropy::Bits256);
+    //** Mnemonic::from_entropy() handles the checksum part which could look like below:
+    //
+    // let hash = sha256::Hash::hash(&entropy);
+    // let entropy_with_checksum = match entropy_type {
+    //     EntropyType::Bits128 => {
+    //         let checksum_4bits = hash[0] >> 4;
+    //         let mut result = entropy.to_vec();
+    //         let last_byte = (checksum_4bits << 4) | 0;
+    //         result.push(last_byte);
+    //         result
+    //     },
+    //     EntropyType::Bits256 => {
+    //         let checksum_8bits = hash[0];
+    //         let mut result = entropy.to_vec();
+    //         result.push(checksum_8bits);
+    //         result
+    //     },
+    // };
+    pub fn generate_recovery_code(entropy_type: &EntropyType) -> Option<String> {
+        let entropy = generate_entropy_bytes(&entropy_type);
         let mnemonic = Mnemonic::from_entropy(&entropy);
-
-        Some(mnemonic.unwrap().words().map(|w| w.into()).collect::<Vec<String>>().join(" ").to_string())
+        Some(
+            mnemonic
+                .unwrap()
+                .words()
+                .map(|w| w.into())
+                .collect::<Vec<String>>()
+                .join(" ")
+                .to_string(),
+        )
     }
 
     fn generate_address() -> Address {
@@ -30,32 +53,32 @@ impl Wallet {
         // 2. store them in local file
         // 3. generate address from saved key
         // 4. return address
-
     }
 
     fn load() -> Wallet {
         todo!()
     }
-
 }
 
-enum Entropy {
+#[derive(Default)]
+pub enum EntropyType {
     Bits128,
+    #[default]
     Bits256,
 }
 
-fn get_entropy(entropy: Entropy) -> Vec<u8> {
+fn generate_entropy_bytes(entropy: &EntropyType) -> Vec<u8> {
     match entropy {
-        Entropy::Bits128 => {
+        EntropyType::Bits128 => {
             let mut e = [0u8; 16];
             OsRng.try_fill_bytes(&mut e).unwrap();
             e.to_vec()
-        },
-        Entropy::Bits256 => {
+        }
+        EntropyType::Bits256 => {
             let mut e = [0u8; 32];
             OsRng.try_fill_bytes(&mut e).unwrap();
             e.to_vec()
-        },
+        }
     }
 }
 
@@ -74,18 +97,26 @@ struct PublicKey {
 
 #[cfg(test)]
 mod test {
-    use crate::Wallet;
+    use crate::{EntropyType, Wallet};
     use bip39::Mnemonic;
 
     #[test]
-    fn mnemonic_generation_roundtrip() {
-        let generated_mnemonic = Wallet::generate_recovery_code();
+    fn mnemonic_from_256bits_generation_roundtrip() {
+        let generated_mnemonic = Wallet::generate_recovery_code(&EntropyType::Bits256);
         assert!(generated_mnemonic.is_some());
         let generated_mnemonic = generated_mnemonic.unwrap();
 
         let parsed_mnemonic = Mnemonic::parse(&generated_mnemonic).unwrap().to_string();
         assert_eq!(generated_mnemonic, parsed_mnemonic);
-        // println!("mnemonic: {:?}", &mnemonic);
     }
 
+    #[test]
+    fn mnemonic_from_128bits_generation_roundtrip() {
+        let generated_mnemonic = Wallet::generate_recovery_code(&EntropyType::Bits128);
+        assert!(generated_mnemonic.is_some());
+        let generated_mnemonic = generated_mnemonic.unwrap();
+
+        let parsed_mnemonic = Mnemonic::parse(&generated_mnemonic).unwrap().to_string();
+        assert_eq!(generated_mnemonic, parsed_mnemonic);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use bitcoin_wallet::{EntropyType, Wallet};
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("WELCOME TO BITCOIN WALLET");
+    println!("Step 1: Generate passphrase");
+    println!("Choose between: 128 / 256 bits");
+    println!("(s) 128 bits");
+    println!("(l) 256 bits");
+    let mut entropy_type_in = String::new();
+    let _ = io::stdin().read_line(&mut entropy_type_in);
+    let entropy_type = match entropy_type_in.trim() {
+        "s" => EntropyType::Bits128,
+        "l" => EntropyType::Bits256,
+        _ => {
+            println!("Defaulting to 256 bits");
+            EntropyType::Bits256
+        },
+    };
+
+    let wordlist = Wallet::generate_recovery_code(&entropy_type)
+        .expect("Failed to generate recovery code");
+    println!("Your passphrase:");
+    println!("=======================================");
+    println!("{}", wordlist);
+    println!("=======================================");
+    println!("Please save it if you ever need to restore your private key");
 }
